c8d53688ec2bacc9c0fb699c6f430ad3befa3ad1
kavinkishore <kavinkishore.n@3edge.in>
2022-09-12 16:42:56 +0530

    sixth commit

--- src/main/java/com/example/main/DemoApplication.java
+++ src/main/java/com/example/main/DemoApplication.java
@@ -8,8 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example")
 public class DemoApplication {
 	
 	public static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
