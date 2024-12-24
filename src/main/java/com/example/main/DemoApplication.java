437be1d07468c7e3b3a23808df823ee825b6aff5
kavinkishore <kavinkishore.n@primesoftinc.com>
2024-12-24 13:07:20 +0530

    Added the version changes

--- src/main/java/com/example/main/DemoApplication.java
+++ src/main/java/com/example/main/DemoApplication.java
@@ -1,14 +1,13 @@
 package com.example.main;
 
 
-
-import javax.annotation.PostConstruct;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @ComponentScan("com.example")
